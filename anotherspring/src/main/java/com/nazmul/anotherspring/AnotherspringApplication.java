d3bf06b9ce60d7d94d6c513793ffd0f184f9f9ac
mdnazmulkarim <mdnazmulkarim@u.boisestate.edu>
2024-03-06 22:00:09 -0700

    java initial project

---
+++ anotherspring/src/main/java/com/nazmul/anotherspring/AnotherspringApplication.java
@@ -0,0 +1,13 @@
+package com.nazmul.anotherspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotherspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnotherspringApplication.class, args);
+	}
+
+}
---
+++ anotherspring/src/test/java/com/nazmul/anotherspring/AnotherspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nazmul.anotherspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotherspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
